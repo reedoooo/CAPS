8ee2662ef65b234ad1d23b36ed82a05a50c8322a
Reed Vogt <readvogt@gmail.com>
2023-05-31 21:25:36 -0700

    add

--- driver/handler.js
+++ driver/handler.js
@@ -1,30 +1,31 @@
 'use strict';
 
-// Importing eventEmitter and eventPool from '../eventPool' module
+require('dotenv').config();
+const { io } = require('socket.io-client');
+const SERVER_URL = process.env.SERVER_URL || 'http://localhost:3001';
+
 const { eventEmitter, eventPool } = require('../eventPool');
 
-// Function to handle the event when a package is delivered to the customer
+const capsSocket = io(SERVER_URL + '/caps');
+
 const packageDeliveredToCustomer = (payload) => {
-  // Logging a success message with the orderId of the delivered package
   console.log(`DRIVER: Successfully delivered package #${payload.orderId}`);
 
-  // Emitting an event with the payload to notify other modules about the package delivery
-  eventEmitter.emit(eventPool[2], payload);
+  capsSocket.emit(eventPool[2], payload);
+  // eventEmitter.emit(eventPool[2], payload)
 };
 
-// Function to handle the event when a package is picked up from the vendor
 const packagePickedUpFromVendor = (payload) => {
-  // Logging a message with the orderId and store from where the package was picked up
   console.log(
     `DRIVER: Package #${payload.orderId} picked up from ${payload.store}`,
   );
 
-  // Emitting an event with the payload to notify other modules about the package pickup
-  eventEmitter.emit(eventPool[1], payload);
+  capsSocket.emit(eventPool[1], payload);
+  // eventEmitter.emit(eventPool[1], payload)
 };
 
-// Exporting the two functions as properties of an object
 module.exports = {
   packageDeliveredToCustomer,
   packagePickedUpFromVendor,
+  capsSocket,
 };
--- driver/index.js
+++ driver/index.js
@@ -4,9 +4,19 @@ const { eventEmitter, eventPool } = require('../eventPool');
 const {
   packageDeliveredToCustomer,
   packagePickedUpFromVendor,
+  capsSocket,
 } = require('./handler');
 
-eventEmitter.on(eventPool[0], (payload) => {
+capsSocket.on(eventPool[0], (payload) => {
+  console.log(
+    'Driver has been notified, en route to pick up package from Vendor.',
+  );
+  capsSocket.emit('join', payload);
   packagePickedUpFromVendor(payload);
   packageDeliveredToCustomer(payload);
 });
+
+// eventEmitter.on(eventPool[0], (payload) => {
+//   packagePickedUpFromVendor(payload);
+//   packageDeliveredToCustomer(payload);
+// });
